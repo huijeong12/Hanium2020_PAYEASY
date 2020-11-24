cad837e76fb38472954248b5c6b4538f60805100
mingyun-ming <57889935+mingyun-ming@users.noreply.github.com>
2020-11-25 04:27:49 +0900

    database 연동

--- app/src/main/java/com/kftc/openbankingsample2/biz/main/MainActivity.java
+++ app/src/main/java/com/kftc/openbankingsample2/biz/main/MainActivity.java
@@ -2,7 +2,13 @@ package com.kftc.openbankingsample2.biz.main;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.kftc.openbankingsample2.R;
 
 /**
@@ -24,6 +30,28 @@ public class MainActivity extends AbstractMainActivity {
         if (args == null) args = new Bundle();
 
         initView();
+
+        // firebase - realtime database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        // database write test
+        myRef.setValue("Hello, World!");
+
+        // database read test
+        myRef.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("pay-easy", "Value is: " + value);
+            }
+
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("pay-easy", "Failed to read value.", error.toException());
+            }
+        });
     }
 
     private void initView() {
