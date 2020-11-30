0c1d5e9bd1978968417b4554bece6e70edbf337f
huijeong12 <choee9712@gmail.com>
2020-11-30 17:42:41 +0900

    final

--- app/src/main/java/com/kftc/openbankingsample2/biz/center_auth/buyer/transfer_withdraw/TransferWithdrawCheck.java
+++ app/src/main/java/com/kftc/openbankingsample2/biz/center_auth/buyer/transfer_withdraw/TransferWithdrawCheck.java
@@ -3,6 +3,7 @@ package com.kftc.openbankingsample2.biz.center_auth.buyer.transfer_withdraw;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +30,7 @@ import java.util.HashMap;
 import java.util.Locale;
 
 public class TransferWithdrawCheck extends AbstractCenterAuthMainFragment {
+
     // context
     private Context context;
 
@@ -77,6 +79,17 @@ public class TransferWithdrawCheck extends AbstractCenterAuthMainFragment {
 
         // 출금이체 요청
         view.findViewById(R.id.btnNext).setOnClickListener(v -> {
+            /*
+                * String[] strings = { QRInfo[2], buyerInfo[0], buyerInfo[1], QRInfo[1], buyerInfo[2],
+                        QRInfo[0], buyerInfo[3], buyerInfo[4], buyerInfo[5],
+                        QRInfo[3], QRInfo[4], QRInfo[5]};
+                * String[] strings = { item.getAccount_type(), item.getAccount_num(), item.getFintech_use_num(),
+                                    item.getAccount_holder_name(), item.getFintech_use_num(), item.getPayer_num(), "TR"};*
+                * String data = total_price + " Pay:easy-입금 한이음분식 김오픈 097 232000067812";
+                * info = { 가게명, cntr_account_type, cntr_account_num, dps_print_content, fintech_use_num, tran_amt,
+                *           req_client_name, req_client_fintech_use_num, req_client_num, transfer_purpose,
+                *           recv_client_name, recv_bank_code, recv_client_account_numt
+             */
 
             // 직전내용 저장
             String accessToken = CenterAuthUtils.getSavedValueFromSetting(CenterAuthConst.CENTER_AUTH_CLIENT_ACCESS_TOKEN);
@@ -87,6 +100,9 @@ public class TransferWithdrawCheck extends AbstractCenterAuthMainFragment {
             Utils.saveData(CenterAuthConst.CENTER_AUTH_FINTECH_USE_NUM, fintechUseNum);
             String reqClientName = info[6];
             Utils.saveData(CenterAuthConst.CENTER_AUTH_REQ_CLIENT_NAME, reqClientName);
+            String withdrawAcc = CenterAuthUtils.getSavedValueFromSetting(CenterAuthConst.CENTER_AUTH_CONTRACT_WITHDRAW_ACCOUNT_NUM);
+            Utils.saveData(CenterAuthConst.CENTER_AUTH_CONTRACT_WITHDRAW_ACCOUNT_NUM, withdrawAcc);
+            withdrawAcc = "8487279403";
 
             // 은행거래고유번호(20자리)
             // 하루동안 유일성이 보장되어야함. 이용기관번호(10자리) + 생성주체구분코드(1자리, U:이용기관, O:오픈뱅킹) + 이용기관 부여번호(9자리)
@@ -94,19 +110,22 @@ public class TransferWithdrawCheck extends AbstractCenterAuthMainFragment {
             String randomUnique9String = Utils.getCurrentTime();    // 이용기관 부여번호를 임시로 시간데이터 사용
             String bankTranId = String.format("%sU%s", clientUseCode, randomUnique9String);
 
+            for (int i = 1; i < 12; i++)
+                Log.d("pay-easy", info[i] + "\n");
+
             // 요청전문
             HashMap<String, String> paramMap = new HashMap<>();
             paramMap.put("bank_tran_id", bankTranId);
-            paramMap.put("cntr_account_type", info[0]);
-            paramMap.put("cntr_account_num", info[1]);
-            paramMap.put("dps_print_content", info[2]);
-            paramMap.put("fintech_use_num", info[3]);
-            paramMap.put("tran_amt", info[4]);
+            paramMap.put("cntr_account_type", info[1]);
+            paramMap.put("cntr_account_num", withdrawAcc);
+            paramMap.put("dps_print_content", info[0]);
+            paramMap.put("fintech_use_num", info[4]);
+            paramMap.put("tran_amt", info[5]);
             paramMap.put("tran_dtime", new SimpleDateFormat("yyyyMMddHHmmss", Locale.KOREA).format(new Date()));
-            paramMap.put("req_client_name", info[5]);
-            paramMap.put("req_client_fintech_use_num", info[6]);
-            paramMap.put("req_client_num", info[7]);
-            paramMap.put("transfer_purpose", info[8]);
+            paramMap.put("req_client_name", info[6]);
+            paramMap.put("req_client_fintech_use_num", info[4]);
+            paramMap.put("req_client_num", clientUseCode);
+            paramMap.put("transfer_purpose", "TR");
             paramMap.put("recv_client_name", info[9]);
             paramMap.put("recv_client_bank_code", info[10]);
             paramMap.put("recv_client_account_num", info[11]);
@@ -117,50 +136,14 @@ public class TransferWithdrawCheck extends AbstractCenterAuthMainFragment {
             CenterAuthApiRetrofitAdapter.getInstance()
                     .transferWithdrawFinNum("Bearer " + accessToken, paramMap)
                     .enqueue(super.handleResponse("tran_amt", "이체완료!! 이체금액", responseJson -> {
-
-                                String BankTranId = setRandomBankTranIdCustom();
-                                String inquiryType = "A";
-                                String inquiryBase = "D";
-                                String fromData = "20180101";
-                                String fromTime = "";
-                                String toDate = Utils.getDateString8(0);
-                                String toTime = "";
-                                String sortOrder = "D";
-                                String tranDtime = new SimpleDateFormat("yyyyMMddHHmmss", Locale.KOREA).format(new Date());
-                                String beforeInquiryTraceInfo = "123";
-
-                                // 요청전문
-                                HashMap<String, String> paramMap2 = new HashMap<>();
-                                paramMap2.put("bank_tran_id", BankTranId);
-                                paramMap2.put("fintech_use_num", fintechUseNum);
-                                paramMap2.put("inquiry_type", inquiryType);
-                                paramMap2.put("inquiry_base", inquiryBase);
-                                paramMap2.put("from_date", fromData);
-                                paramMap2.put("from_time", fromTime);
-                                paramMap2.put("to_date", toDate);
-                                paramMap2.put("to_time", toTime);
-                                paramMap2.put("sort_order", sortOrder);
-                                paramMap2.put("tran_dtime", tranDtime);
-                                paramMap2.put("befor_inquiry_trace_info", beforeInquiryTraceInfo);
-
-                                showProgress();
-
-                                // Retrofit 이용하여 거래 내역 조회 API 호출
-                                CenterAuthApiRetrofitAdapter.getInstance()
-                                        .accountTrasactionListFinNum("Bearer " + accessToken, paramMap2)
-                                        .enqueue(super.handleResponse("page_record_cnt", "현재페이지 조회건수", responseJson2 -> {
-
-                                                    // 성공하면 결과화면으로 이동
-                                                    ApiCallAccountTransactionResponse result =
-                                                            new Gson().fromJson(responseJson2, ApiCallAccountTransactionResponse.class);
-                                                    args.putParcelable("result", result);
-                                                    args.putSerializable("request", paramMap2);
-                                                    args.putString(CenterAuthConst.BUNDLE_KEY_ACCESS_TOKEN, accessToken);
-
-                                                    // 거래 내역 조회 창으로 이동
-                                                    startFragment(HomeFragment.class, args, R.string.fragment_id_api_call_transaction);
-                                                })
-                                        );
+                                Bundle sendingArgs = new Bundle();
+                                /*
+                                * 가게명, 받는 사람, 수취 계좌, 거래 금액, 보낸 사람 핀테크 이용 번호
+                                 */
+                                String[] transferResultInfo = { info[0], info[9], info[11], info[5], info[4] };
+                                Bundle bundle = new Bundle();
+                                bundle.putStringArray("trans_info", transferResultInfo);
+                                startFragment(TransferWithdrawResult.class, bundle, R.string.fragment_id_transfer_withdraw_result);
                             })
                     );
 
